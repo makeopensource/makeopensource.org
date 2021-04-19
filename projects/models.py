82414452089e24958338889a944fcb3e29b960b9
Emil Kovacev <emilkova@buffalo.edu>
2021-04-19 18:07:30 -0400

    completed projects app functionality

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Contributor(models.Model):
+	name = models.CharField(max_length=200)
+	email = models.EmailField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+class Project(models.Model):
+	title = models.CharField(max_length=200)
+	description = models.TextField(max_length=2000)
+	contributors = models.ManyToManyField(Contributor)
+	release_date = models.DateField(auto_now_add=True)
+	github = models.CharField('github link', max_length=500)
+
+	def __str__(self):
+		return self.title
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Project
 
 def index(request):
-	return render(request, 'projects/index.html')
+	projects = {'projects': list(Project.objects.order_by('-release_date'))}
+	return render(request, 'projects/index.html', projects)
