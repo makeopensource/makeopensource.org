3838473d78272730cac40c4cde4edcb6cb4f7242
Emil Kovacev <emilkova@buffalo.edu>
2021-04-21 11:18:32 -0400

    added global templates and static files

--- mos/settings.py
+++ mos/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'mos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, "static"),
+]
