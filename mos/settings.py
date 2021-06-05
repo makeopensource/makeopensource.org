cd76532ddfaafa6b9c0663d2a8139c5e842be098
Emil Kovacev <emilkova@buffalo.edu>
2021-06-05 12:34:33 -0400

    added API throttle rate and pagination

--- mos/settings.py
+++ mos/settings.py
@@ -134,3 +134,21 @@ STATICFILES_DIRS = [
 # Models
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
+# DJANDO REST FRAMEWORK
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/minute',
+        'user': '10/minute'
+    }
+}
