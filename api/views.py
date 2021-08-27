9ba2d89ea2a083a769aa0157f3dd514b70cfb06f
Emil Kovacev <emilkova@buffalo.edu>
2021-08-27 11:16:07 -0400

    Added custom html responses for Errors and Verification

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def page_not_found(request, exception):
+    return render(request, 'api/page_not_found.html')
+
+def error(request):
+    return render(request, 'api/page_not_found.html')
