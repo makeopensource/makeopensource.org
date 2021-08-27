77c08461f7780afd6f9a76a5e55b4008eeb85ee9
Nick Brown <njbrown4@buffalo.edu>
2021-08-26 22:28:12 -0400

    fixed static files for admin page

--- mos/settings.py
+++ mos/settings.py
@@ -165,10 +165,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = '/home/mos/repos/makeopensource.org/static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#	os.path.join(BASE_DIR, "static"),
+#]
 
 
 # Models
