bfb13ff6eaf2371870dc92613647e2a9c21b470d
Nicholas Myers <32116122+NicholasMy@users.noreply.github.com>
2026-08-18 16:50:57 -0400

    Add Discord redirect endpoint and header link, update homepage with basic club info

--- web/makeopensource/general/general.py
+++ web/makeopensource/general/general.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 general_bp = Blueprint("general", __name__)
 
@@ -6,3 +6,8 @@ general_bp = Blueprint("general", __name__)
 @general_bp.get("/")
 def index():
     return render_template("general/homepage.html")
+
+
+@general_bp.get("/discord/")
+def discord():
+    return redirect("https://discord.com/invite/xbBPqdqr6n")
